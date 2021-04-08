9937d386c909d9acca4710ad9f430867052e2e0a
Vivek Tadiparthi <saivivekreddy.s6@gmail.com>
2021-04-08 18:56:56 -0500

    screen shot 6 server resources

--- controllers/Soda.js
+++ controllers/Soda.js
@@ -1,21 +1,26 @@
 var Soda = require('../models/Soda');
-// List of all Sodas
-exports.Soda_list = function(req, res) {
-res.send('NOT IMPLEMENTED: Soda list');
+// List of all Soda
+exports.Soda_list = async function (req, res) {
+    try {
+        theSoda = await Soda.find();
+        res.send(theSoda);
+    } catch (err) {
+        res.error(500, `{"error": ${err}}`);
+    }
 };
 // for a specific Soda.
-exports.Soda_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: Soda detail: ' + req.params.id);
+exports.Soda_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: Soda detail: ' + req.params.id);
 };
 // Handle Soda create on POST.
-exports.Soda_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: Soda create POST');
+exports.Soda_create_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: Soda create POST');
 };
 // Handle Soda delete form on DELETE.
-exports.Soda_delete = function(req, res) {
-res.send('NOT IMPLEMENTED: Soda delete DELETE ' + req.params.id);
+exports.Soda_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: Soda delete DELETE ' + req.params.id);
 };
 // Handle Soda update form on PUT.
-exports.Soda_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: Soda update PUT' + req.params.id);
+exports.Soda_update_put = function (req, res) {
+    res.send('NOT IMPLEMENTED: Soda update PUT' + req.params.id);
 }
