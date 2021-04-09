7e15d2aa350bcf5d346dc011faac09c6bf809834
Vivek Tadiparthi <saivivekreddy.s6@gmail.com>
2021-04-08 19:15:42 -0500

    final push

--- controllers/Soda.js
+++ controllers/Soda.js
@@ -26,8 +26,22 @@ exports.Soda_detail = function (req, res) {
     res.send('NOT IMPLEMENTED: Soda detail: ' + req.params.id);
 };
 // Handle Soda create on POST.
-exports.Soda_create_post = function (req, res) {
-    res.send('NOT IMPLEMENTED: Soda create POST');
+exports.Soda_create_post = async function (req, res) {
+    console.log(req.body)
+    let document = new Soda();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"Sodatype":"goat", "cost":12, "size":"large"}
+    document.company = req.body.company;
+    document.flavour = req.body.flavour;
+    document.price = req.body.price;
+    try {
+        let result = await document.save();
+        res.send(result);
+    } catch (err) {
+        res.error(500, `{"error": ${err}}`);
+    }
 };
 // Handle Soda delete form on DELETE.
 exports.Soda_delete = function (req, res) {
